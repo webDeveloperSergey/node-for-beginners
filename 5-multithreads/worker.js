8305ca8f9d3052eb7fd5c75d594784ba29d423ee
Sergei Kultyshkin <kultyshkin2003@gmail.com>
2025-03-20 08:55:23 +0300

    Влил не туда (ошибка)

--- 5-multithreads/worker.js
+++
@@ -1,7 +0,0 @@
-const { parentPort, workerData } = require('worker_threads')
-
-const countDivisibleByThree = ({ chunk }) => {
-  return chunk.filter((item) => item % 3 === 0).length
-}
-
-parentPort.postMessage(countDivisibleByThree(workerData))
